8dab4432423e779fd0e5136ca966f6ed8fd2f96a
strollingorange <denghaojin@gmail.com>
2022-06-21 21:57:15 -0400

    update eval per batch

--- tf2/run.py
+++ tf2/run.py
@@ -649,6 +649,11 @@ def main(argv):
           checkpoint_manager.save(cur_step)
           logging.info('Completed: %d / %d steps', cur_step, train_steps)
           metrics.log_and_write_metrics_to_summary(all_metrics, cur_step)
+
+          perform_evaluation(model, builder, eval_steps,
+                             checkpoint_manager.latest_checkpoint, strategy,
+                             topology)
+
           tf.summary.scalar(
               'learning_rate',
               learning_rate(tf.cast(global_step, dtype=tf.float32)),
